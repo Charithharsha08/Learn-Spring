eab6d856865459c97ea1020342a342ae6618174a
Charith Harsha <147936344+Charithharsha08@users.noreply.github.com>
2025-02-05 10:19:43 +0530

    different about @controller and @RestController

--- 08.Spring_Web_MVC/src/main/java/lk/ijse/controller/HelloController.java
+++ 08.Spring_Web_MVC/src/main/java/lk/ijse/controller/HelloController.java
@@ -1,10 +1,11 @@
 package lk.ijse.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 @RequestMapping("hello")
 public class HelloController {
     public HelloController() {
@@ -12,11 +13,8 @@ public class HelloController {
     }
     @GetMapping
     public String sayHello(){
-        return "Hello World";
+        return "index";
     }
 
-    @GetMapping("bye")
-    public String sayBye(){
-        return "Bye World";
-    }
+
 }
